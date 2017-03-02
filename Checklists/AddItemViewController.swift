3eb66471562e0d3a1a3ab1ca75defb2f2732c147
Pavel Prokofyev <feorand@gmail.com>
2017-03-02 15:21:26 +0500

    Added basic functions to Cancel and Done in NewitemViewController

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,28 +8,18 @@
 
 import UIKit
 
-class AddItemViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class AddItemViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension AddItemViewController {
+    @IBAction func cancelPressed() {
+        dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func donePressed() {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
 }
