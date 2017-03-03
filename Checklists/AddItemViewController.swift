bf9f29216d42f339afe9ee86b8bc34ed5c80004a
Pavel Prokofyev <feorand@gmail.com>
2017-03-03 13:58:33 +0500

    Added Done button protection in AdditemViewController

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
     
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
@@ -30,5 +31,17 @@ extension AddItemViewController {
         super.viewWillAppear(animated)
         
         textField.becomeFirstResponder()
+    }
+}
+
+extension AddItemViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        
+        doneButton.isEnabled = (newText.length > 0)
+        
+        return true
     }
 }
